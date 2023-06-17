82b24900b2fe8e853b0dc940f6d4cb8ad5933ceb
DEL <sahu19051@gmail.com>
2023-06-17 16:10:03 +0530

    Solution Added

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -3,11 +3,26 @@ package com.driver;
 public class BankAccount {
 
     private String name;
-    private double balance;
+    double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
 
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance  = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
@@ -15,17 +30,48 @@ public class BankAccount {
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        return null;
+        int rem = sum;
+        String accountNo = "";
+        if(digits*9 < sum){
+            throw new Exception("Account Number can not be generated");
+        }
+        else{
+            while(digits >0 && rem >0){
+                if(rem >= 9){
+                    rem = rem - 9;
+                    accountNo = accountNo +"9";
+                }
+                else{
+                    accountNo = accountNo + Integer.toString(rem);
+                    rem = 0;
+                }
+                digits--;
+            }
+            while(digits > 0){
+                accountNo = accountNo + "0";
+                digits--;
+            }
+            return accountNo;
+        }
+       // return null;
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance += amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
+        if(this.balance-amount >= getMinBalance()){
+            balance= balance - amount;
+
+        }
+        else{
+            throw  new Exception("Insufficient Balance");
+        }
     }
 
 }
